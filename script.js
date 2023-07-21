6764fe22551793731be6e878b1b79e9866610bde
Jackson <jacksonmagno59@gmail.com>
2023-07-21 11:41:34 -0300

    teste

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-let num1 = 5
-let num2 = 6
-if(num1 > num2){
-    console.log(`${num1} maior que ${num2}`)
-} else{
-    console.log(`${num2} maior que ${num1}`)
-}
+//let num1 = 5
+//let num2 = 6
+//if(num1 > num2){
+ //   console.log(`${num1} maior que ${num2}`)
+//} else{
+   // console.log(`${num2} maior que ${num1}`)
+//}
 
+
+// function multiplicar(funcao){
+    // return function(array){
+        // return funcao.apply(null,array);
+    // };
+// }
+// var multiplicarElementosArray = multiplicar(function(x, y){ return x * y});
+// multiplicarElementosArray([2, 2])
+
+
+
+// function principal(argumento){
+    // console.log('o resultado é de ${argumento}');
+// }
+// function callback(a,b,callback){
+    // let op = a + b;
+// }
+//  callback(5,6,principal);
+
+
+
+// async function obterDadosAssincronos(url, callback){
+    // const resultados = await fetch(url);
+// const dados = await resultados.json();
+// callback(dados);
+// };
+
+// function manipularDadosAssicronos(dados){
+    // console.log(dados);
+// }
+// obterDadosAssincronos('https://minha.api.com/dados',manipularDadosAssicronos);
 
---
+++ script2.js
@@ -0,0 +1,7 @@
+let num1 = 4
+let num2 = 8
+if(num1 > num2){
+    console.log(`${num1} maior que ${num2}`)
+} else{
+    console.log(`${num2} maior que ${num1}`)
+}
